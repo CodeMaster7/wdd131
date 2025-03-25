81dd168c93f3cd319e7169777d39b9794f34e668
Sam <sammybonfanti@gmail.com>
2025-03-24 21:34:04 -0700

    Update temple image URLs to use relative paths

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -9,70 +9,70 @@ const temples = [
 		location: 'Aba, Nigeria',
 		dedicated: '2005, August, 7',
 		area: 11500,
-		imageUrl: '../images/aba-nigeria-temple.webp'
+		imageUrl: 'images/aba-nigeria-temple.webp'
 	},
 	{
 		templeName: 'Manti Utah',
 		location: 'Manti, Utah, United States',
 		dedicated: '1888, May, 21',
 		area: 74792,
-		imageUrl: '../images/manti-temple.webp'
+		imageUrl: 'images/manti-temple.webp'
 	},
 	{
 		templeName: 'Payson Utah',
 		location: 'Payson, Utah, United States',
 		dedicated: '2015, June, 7',
 		area: 96630,
-		imageUrl: '../images/payson-utah-temple.webp'
+		imageUrl: 'images/payson-utah-temple.webp'
 	},
 	{
 		templeName: 'Yigo Guam',
 		location: 'Yigo, Guam',
 		dedicated: '2020, May, 2',
 		area: 6861,
-		imageUrl: '../images/yigo-guam-temple.webp'
+		imageUrl: 'images/yigo-guam-temple.webp'
 	},
 	{
 		templeName: 'Washington D.C.',
 		location: 'Kensington, Maryland, United States',
 		dedicated: '1974, November, 19',
 		area: 156558,
-		imageUrl: '../images/washington-dc-temple.webp'
+		imageUrl: 'images/washington-dc-temple.webp'
 	},
 	{
 		templeName: 'Lima Perú',
 		location: 'Lima, Perú',
 		dedicated: '1986, January, 10',
 		area: 9600,
-		imageUrl: '../images/lima-peru-temple.webp'
+		imageUrl: 'images/lima-peru-temple.webp'
 	},
 	{
 		templeName: 'Mexico City Mexico',
 		location: 'Mexico City, Mexico',
 		dedicated: '1983, December, 2',
 		area: 116642,
-		imageUrl: '../images/mexico-city-temple.webp'
+		imageUrl: 'images/mexico-city-temple.webp'
 	},
 	{
 		templeName: 'Rome Italy',
 		location: 'Rome, Italy',
 		dedicated: '2010, October, 23',
 		area: 41010,
-		imageUrl: '../images/rome-temple.webp'
+		imageUrl: 'images/rome-temple.webp'
 	},
 	{
 		templeName: 'Laie Hawaii',
 		location: 'Laie, Hawaii, United States',
 		dedicated: '1919, November, 27-30',
 		area: 42100,
-		imageUrl: '../images/laie-temple-exterior.webp'
+		imageUrl: 'images/laie-temple-exterior.webp'
 	},
 	{
 		templeName: 'Bern Switzerland',
 		location: 'Bern, Switzerland',
 		dedicated: '1955, September, 11-15',
 		area: 35546,
-		imageUrl: '../images/ber-switzerland.webp'
+		imageUrl: 'images/ber-switzerland.webp'
 	}
 ]
 
