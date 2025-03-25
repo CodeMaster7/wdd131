4524ef00982709ffced10e03a78b1a3fd5b8f294
Sam <sammybonfanti@gmail.com>
2025-03-24 20:59:15 -0700

    Update temple image URLs to local paths

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -9,80 +9,70 @@ const temples = [
 		location: 'Aba, Nigeria',
 		dedicated: '2005, August, 7',
 		area: 11500,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg'
+		imageUrl: '../images/aba-nigeria-temple.jpg'
 	},
 	{
 		templeName: 'Manti Utah',
 		location: 'Manti, Utah, United States',
 		dedicated: '1888, May, 21',
 		area: 74792,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg'
+		imageUrl: '../images/manti-temple.jpg'
 	},
 	{
 		templeName: 'Payson Utah',
 		location: 'Payson, Utah, United States',
 		dedicated: '2015, June, 7',
 		area: 96630,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg'
+		imageUrl: '../images/payson-utah-temple.jpg'
 	},
 	{
 		templeName: 'Yigo Guam',
 		location: 'Yigo, Guam',
 		dedicated: '2020, May, 2',
 		area: 6861,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg'
+		imageUrl: '../images/yigo-guam-temple.jpg'
 	},
 	{
 		templeName: 'Washington D.C.',
 		location: 'Kensington, Maryland, United States',
 		dedicated: '1974, November, 19',
 		area: 156558,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg'
+		imageUrl: '../images/washington-dc-temple.jpeg'
 	},
 	{
 		templeName: 'Lima Perú',
 		location: 'Lima, Perú',
 		dedicated: '1986, January, 10',
 		area: 9600,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg'
+		imageUrl: '../images/lima-peru-temple.jpg'
 	},
 	{
 		templeName: 'Mexico City Mexico',
 		location: 'Mexico City, Mexico',
 		dedicated: '1983, December, 2',
 		area: 116642,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg'
+		imageUrl: '../images/mexico-city-temple.jpg'
 	},
 	{
 		templeName: 'Rome Italy',
 		location: 'Rome, Italy',
 		dedicated: '2010, October, 23',
 		area: 41010,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/rome-italy/2019/400x250/2-Rome-Temple-2190090.jpg'
+		imageUrl: '../images/rome-temple.jpg'
 	},
 	{
 		templeName: 'Laie Hawaii',
 		location: 'Laie, Hawaii, United States',
 		dedicated: '1919, November, 27-30',
 		area: 42100,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/laie-hawaii/400x250/laie-temple-775369-wallpaper.jpg'
+		imageUrl: '../images/laie-temple.jpg'
 	},
 	{
 		templeName: 'Bern Switzerland',
 		location: 'Bern, Switzerland',
 		dedicated: '1955, September, 11-15',
 		area: 35546,
-		imageUrl:
-			'https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/bern-switzerland/400x250/bern-switzerland-temple-lds-653038-wallpaper.jpg'
+		imageUrl: '../images/ber-switzerland.jpg'
 	}
 ]
 
